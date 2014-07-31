b368aa211533db12ba394a295b177843e808711c
rasbabbo <rasbabbo@yahoo.com>
2014-07-30 19:18:34 -0700

    fourth stab at groundwork

--- app.js
+++ app.js
@@ -13,13 +13,44 @@ app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
-// logging
+// logging every request
 app.use(function(req, res, next){
   console.log(req.method, req.url)
   next()
 });
 
+
+//logging
+app.get('/posts/new', function(req, res) {
+  res.render('posts/new');
+})
+
+app.post('/posts', function(req, res) {
+  console.log("inside /post posts")
+
+  db.user.findOrCreate({username: req.body.username, password: req.body.password})
+
+    .error(function(err) {
+      res.send(err);
+    })
+
+    .success(function(user) {
+      // res.send(user);
+      var newPost = db.post.build({title: req.body.title, body: req.body.body});
+        user.addPost(newPost).success(function(post) {
+        req.send(post);
+      })
+    });
+  // console.log(req.body);
+  // res.redirect('/');
+});
+
+
 app.get('/users', function (req,res) {
+  db.user.findAll()
+  .success(function(allUsers){
+    res.render('users/index', {user: allUsers});
+  })
 //
 });
 
--- console.js
+++ console.js
@@ -3,4 +3,29 @@ var db = require('./models/index.js');
 var pkge = require("./package")
 var newREPL = repl.start(pkge.name + " > ");
 
-newREPL.context.db = db;
+
+// db.user.create
+// db.user.findAll().success(function(user){
+//   console.log(user);
+// });
+
+
+// db.post.create({title: "blah", body:"stuff"});
+
+// .find
+// .findAll
+// .create //save right after
+// .build && .save
+// .findOrCreate //will find and if not there create
+// .addNAMEOFYOURMODEL
+// .addPost
+
+
+db.user.find({id:1}).success(function(user) {
+  console.log(user);
+});
+
+
+
+
+// newREPL.context.db = db;
