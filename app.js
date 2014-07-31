c8ff76b6c0c4514a5c3236296994b49708ac32e4
rasbabbo <rasbabbo@yahoo.com>
2014-07-30 22:59:24 -0700

    moving

--- app.js
+++ app.js
@@ -15,31 +15,80 @@ app.use(express.static(__dirname + '/public'));
 
 // logging every request
 app.use(function(req, res, next){
-  console.log(req.method, req.url)
-  next()
+  console.log(req.method, req.url);
+  next();
 });
+
+//AUTHENTICATION
+
+app.get('/', function(req, res) {
+  if(!req.user) {
+    res.render('signup');
+  } else {
+    res.redirect('home');
+  }
+});
+
+
+app.get('signup', function(req, res) {
+  if(!req.user) {
+    res.render('signup', {message: null, username: ""});
+  } else {
+    (res.redirect('/login'));
+  }
+})
+
+app.get('/login', function(req, res) {
+  res.render('login', {message: null})
+});
+
+app.post('/login', function(req, res) {
+  db.user.authorize(req.body.username, req.body.password,
+    function(err) {
+      res.render('login', {message: err.message})
+      }, function(success) {
+        res.render('home', {message: success.message});
+      })
+    });
+
+// app.post('/create', function(req, res) {
+//   db.user.createNewUser(req.body.username, req.body.password,
+//     function(err) {
+//       res.render('login', {message: err.message, username: req.body.username})
+//       res.redirect('home');
+//     });
+// });
+
+// app.get('home', function(req, res) {
+//   res.render('home', {isAuthenticated: req.isAuthenticated(),
+//     user: req.user
+//   });
+// });
+
+// app.get('*', function(){
+//   res.render('404');
+// });
+
+
 
 
 //logging
 app.get('/posts/new', function(req, res) {
   res.render('posts/new');
-})
+});
 
 app.post('/posts', function(req, res) {
   console.log("inside /post posts")
-
   db.user.findOrCreate({username: req.body.username, password: req.body.password})
-
     .error(function(err) {
       res.send(err);
     })
-
     .success(function(user) {
       // res.send(user);
       var newPost = db.post.build({title: req.body.title, body: req.body.body});
         user.addPost(newPost).success(function(post) {
-        req.send(post);
-      })
+        res.send(post);
+      });
     });
   // console.log(req.body);
   // res.redirect('/');
@@ -50,28 +99,60 @@ app.get('/users', function (req,res) {
   db.user.findAll()
   .success(function(allUsers){
     res.render('users/index', {user: allUsers});
-  })
+  });
 //
 });
 
 app.get('/users/:id', function (req,res) {
-  //
+  var id = req.params.id;
+  db.user.find(3)
+  .success(function(foundUser){
+    foundUser.getPosts() 
+     .success(function(foundPosts) {
+      res.render('users/show', {
+        user: foundUser,
+        posts: foundPosts
+      });
+    })
+  });
 });
+
 
 app.get('/posts/:id', function (req,res) {
   var id = req.params.id;
-  //
-
+  db.posts.find(id)
+  .success(function(foundPost) {
+    res.render('posts/show', {
+      post:foundPost
+    });
+  });
 });
 
 app.get('/users/:id/posts/new', function(req, res){
   var id = req.params.id;
-  //
+  db.user.find.id
+  .success(function(foundUser) {
+    res.render('posts/new', {
+      user: foundUser
+    });
+  });
 });
 
 app.post('/users/:id/posts', function(req, res){
   var id = req.params.id;
-  //
+  db.user.find(id)
+  .success(function(foundUser) {
+    db.post.create(req.body.post)
+     .success(function(newPost){
+      foundUser.addPost(newPost)
+       .success(function(){
+        res.redirect('/posts/' + newPost.dataValues.id);
+       });
+     });
+    })
+  .error(function(err){
+    res.redirect('/users');
+  });
 });
 
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
-var bcrypt = require("bcrypt");
-var salt = bcrypt.genSaltSync(10);
+var bcrypt = require("bcrypt"),
+ salt = bcrypt.genSaltSync(10),
+ passport = require('passport'),
+ passportLocal = require('passport-local');
 
 module.exports = function (sequelize, DataTypes){
    var User = sequelize.define('user', {
@@ -20,9 +22,60 @@ module.exports = function (sequelize, DataTypes){
     
     {
       classMethods: {
-        associate: function(db){
-          User.hasMany(db.post);
-        }
+        encryptPass: function(password){
+          var hash = bcrypt.hashSync(password, salt);
+          return hash;
+          },
+      //compare a password
+        comparePass: function(userpass, dbpass) {
+        //take care not to salt twice
+          return bcrypt.compareSync(userpass, dbpass);
+        //returns true or false
+          },
+
+        createNewUser: function(username, password, err, success) {
+          if (password.length < 6) {
+            err({message: "Password should be more than 6 characters, duh"});
+              } else {
+                  User.create({
+                  username: username,
+                  password: User.encryptPass(password)
+                  }).error(function(error){
+                  console.log(error);
+                if(error.username) {
+              //the error is defined by the validate objects in username definition
+                  err({message: "Your username has to be 6 characters or longer"});
+               } else {
+                  err({message: "Someone or thing has already used this name - pick another"});
+                  }
+                   }).success(function(user){
+                  success({message: "Account created, I guess you could log in"});
+                });
+              }
+            },
+
+        authorize: function(username, password, err, success) {
+          //find user in the db
+          User.find({
+            where: { username: username }
+            })
+            //when that is complete...
+            //the .done is used b/c of the asynchronous nature of JS
+          .done(function(error, user) {
+          if(err) {
+            console.log(error);
+            err({message: "Dang, something's messed up"});
+          } else if (user === null) {
+              err({message: "Username doesn't exist"});
+              //these are called from the above defined functions
+              //if not true then password is problem
+          } else if ((User.comparePass(password, user.password)) === true) {
+            success();
+          } else {
+            err({message: "Password is wrong"});
+          }
+        });
+      }
       }
     } //close classMethods outer 
 
